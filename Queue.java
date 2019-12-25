983ddf73f00e8a2b66068b9ff22b805544efd0f8
minhradz <minhradz@gmail.com>
2019-12-25 16:03:31 -0500

    Resolved the fixed type cast warning by not using java generics, some code refinement.

--- Queue.java
+++ Queue.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Queue<E> {
+public class Queue{
 /*
             * front is the index of the first item on the queue
             * back  is the index of the last item on the queue
@@ -28,10 +28,8 @@ public class Queue<E> {
  */
     private int capacity = 4;
     int size = 0;
-    // C-Style array declaration + unchecked cast
-    //FIXME -- try to find a new way to do the declaration
-    //      -- and a way to prevent unchecked casting
-    private E items[] = (E[]) new Object[capacity];
+
+    private int[] items = new int[capacity];
 
     // front is the index of the first item on the queue
     private int front = 0;
@@ -43,7 +41,7 @@ public class Queue<E> {
         return size;
     }
 
-    public void enqueue(E item) {
+    public void enqueue(int item) {
         if (size == capacity)
             expandCapacity();
         System.out.println("Enqueueing " + item);
@@ -52,9 +50,9 @@ public class Queue<E> {
         items[back] = item;
     }
 
-    public E dequeue() throws EmptyQueueException {
+    public int dequeue() throws EmptyQueueException {
         if (size == 0) throw new EmptyQueueException();
-        E answer = items[front++];
+        int answer = items[front++];
         size--;
         if (size == 0) {
             front = 0;
@@ -64,16 +62,16 @@ public class Queue<E> {
         return answer;
     }
 
-    public E peek() throws EmptyQueueException {
+    public int peek() throws EmptyQueueException {
         if (size == 0) throw new EmptyQueueException();
         return items[front];
     }
 
     private void expandCapacity() {
-        System.out.println("Calling expandCapacity");
+        System.out.println("Calling expandCapacity. The array will now be doubled in size. The old size is " + size);
         int oldCapacity = capacity;
         capacity *= 2;
-        E[ ] newItems = (E [ ]) new Object[capacity];
+        int[] newItems = new int[capacity];
         for (int i=0; i<size; i++) {
             newItems[i] = items[front++];
             if (front == oldCapacity) front = 0;
@@ -85,14 +83,14 @@ public class Queue<E> {
 
     public String toString() {
         if (size == 0)
-            return "[ ]" + " front " + front + " back " + back + " size " + size + " capacity " + capacity;
+            return "[ ]" + " front: " + front + " || back: " + back + " || size: " + size + " || capacity: " + capacity;
         String answer = "[ ";
         int position = front;
         for (int i=0; i<size; i++) {
             answer += items[position++] + " ";
             if (position == capacity) position = 0;
         }
-        return answer + "] front " + front + " back " + back + " size " + size + " capacity " + capacity;
+        return answer + "] front: " + front + " || back: " + back + " || size: " + size + " ||capacity: " + capacity;
     }
 
     private static class EmptyQueueException extends Exception{
@@ -108,24 +106,32 @@ public class Queue<E> {
     }
 
     public static void main(String[ ] args) throws EmptyQueueException {
-        Queue<Integer> q = new Queue<Integer>();
+
+        Queue q = new Queue();
         Scanner console = new Scanner(System.in);
         int choice;
         int number;
         System.out.println(q);
+
         while (true) {
             System.out.println("Enter 1 to enqueue, 2 to dequeue, 3 to stop");
+
             choice = console.nextInt();
+
             switch (choice) {
+
                 case 3: System.exit(0);
+
                 case 1: System.out.println("Enter the number to enqueue");
                     number = console.nextInt();
                     q.enqueue(number);
                     System.out.println(q);
                     break;
+
                 case 2: System.out.println(q.dequeue() + " is dequeued");
                     System.out.println(q);
                     break;
+
                 default: System.out.println("Invalid input");
             }
         }
